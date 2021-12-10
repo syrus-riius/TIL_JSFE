deacb561c0a7aba248a3cc6de878085533109912
syrusriius <syrusriius@gmail.com>
2021-12-11 01:57:52 +0900

    date

--- ES6/07_variable.js
+++ ES6/07_variable.js
@@ -1,4 +1,5 @@
-// 변수의 특징 : 선언, 할당, 범위
+// 변수의 특징 : 선언, 할당, 범위, 호이스팅
+
 
 //const, let은 재선언이 금지됨.
 //const, let의 스코프는 블록스코프{} (var : 함수스코프)
@@ -21,3 +22,31 @@ Object.freeze(obj)
 
 obj.name = 'lee'
 console.log(obj)
+
+//호이스팅 : 변수/함수의 선언을 맨 위로 끌고 오고 할당은 후에 한다.
+
+
+
+// 전역변수
+
+// window.변수명 = '값' 형태로 브라우저에서 전역변수 설정 가능
+
+
+// 예제
+
+if (true) {
+    var b = 2;
+    if (true) {
+        let b = 3;
+    }
+    console.log(b); // let의 스코프는 {}블록이기 때문에 출력은 2로 된다.
+
+}
+
+for (var i = 0; i < 5; i++) { 
+    setTimeout(function() { console.log(i); }, i*1000 ); 
+}
+
+// 5가 5번 나오는 이유는
+// var가 5번 미리 다 돌았기 때문임
+// 해결을 위해서는 블록스코프인 let을 사용한다.
---
+++ ES6/08_templet_literal.js
@@ -0,0 +1,27 @@
+//백틱 사용하여 문자열 작성 -> 엔터키, 변수 구멍을 통해서 사용가능
+
+let sonny = 'HEUNG MIN SON';
+let totNo7 = `
+tottenham 
+no.7 
+${sonny}`;
+
+console.log(totNo7);
+
+// 태그드 리터럴
+
+function func1() {
+    return 10;
+}
+
+func1(); // 원래는 이렇게 써야하지만
+func1`` //이렇게 해서 사용 가능
+
+function 해체분석기(문자들, 변수들){
+    console.log(문자들); //[ 'Hello, Iam ', '. Good to see you.' ]
+    console.log(변수들); //HEUNG MIN SON
+}
+
+해체분석기`Hello, Iam ${sonny}. Good to see you.`;
+
+// 태그드 리터럴 양 옆을 배열에 저장.
